7061b106a90ee6cdd7082d0c8849e785bd5ae90b
Anders Steinlein <anders@e5r.no>
2018-09-12 00:58:13 +0200

    Remove old apis package and update auto-generated tests
    apis are now renamed to api, so the old files are no longer needed.

--- mailmojo/apis/__init__.py
+++
@@ -1,7 +0,0 @@
-from __future__ import absolute_import
-
-# import apis into api package
-from .accounts_api import AccountsApi
-from .contacts_api import ContactsApi
-from .embed_api import EmbedApi
-from .lists_api import ListsApi
